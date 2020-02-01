3a66e002a24f982a20a251a13387b3598c0f4d1f
Nicola Halim <staticddq@gmail.com>
2020-02-02 03:17:59 +1100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,6 +19,17 @@ app.use('/api/update', require('./api/update'));
 
 app.listen(PORT, () => console.log("Listening on port " + PORT));
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 db.query('SELECT NOW()', (err, res) => {
     if (err.error)
         return console.log(err.error);
